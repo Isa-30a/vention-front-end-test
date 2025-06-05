4cec790981cf434fd684912976ae3d50e6262bdd
Isa-30a <isabella.alvasol@gmail.com>
2025-06-05 15:55:31 -0500

    ♻️ create a new fucntion to adding an image to an element

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,8 +2,6 @@ const cardListImg=["assets/blue-flower.png","assets/orange-flower.png", "assets/
 
 const layout = document.querySelector(".layout");
 
-const img = document.querySelector("img");
-
 addCards(cardListImg, layout);
 
 function addCards(cardListImg, container){
@@ -12,12 +10,15 @@ function addCards(cardListImg, container){
         const imgSrc = cardListImg[index];
         const newDiv = document.createElement("div");
         newDiv.setAttribute("class", "card");
-
-        container.appendChild(newDiv)
-        const img = document.createElement("img");
         
-        img.setAttribute("src",imgSrc);
+        addImgToanElement(container,imgSrc)
         newDiv.appendChild(img);
     }
-   
+}
+
+function addImgToanElement(element,src){
+    element.appendChild(newDiv)
+    const img = document.createElement("img");
+        
+    img.setAttribute("src",src);
 }
