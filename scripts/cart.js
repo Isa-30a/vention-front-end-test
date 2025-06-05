56b3831bcabbfcce95bdccc112d5bfcb297b7243
Isa-30a <isabella.alvasol@gmail.com>
2025-06-05 16:05:36 -0500

    add a CreateCard function and add temporary flowers data

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,5 +1,24 @@
 const cardListImg=["assets/blue-flower.png","assets/orange-flower.png", "assets/pink-flower.png"];
-
+const tempFlowers = [
+    {
+        img: cardListImg[0],
+        name: "Blue Flower",
+        price: "80.00",
+        stars:4
+    },
+    {
+        img: cardListImg[1],
+        name: "Orange Flower",
+        price: "17.60",
+        stars:3
+    },
+    {
+        img: cardListImg[2],
+        name: "Pink Flower",
+        price: "40.00",
+        stars:5
+    }
+]
 const layout = document.querySelector(".layout");
 
 addCards(cardListImg, layout);
@@ -8,13 +27,20 @@ function addCards(cardListImg, container){
 
     for (let index = 0; index < cardListImg.length; index++) {
         const imgSrc = cardListImg[index];
-        const newDiv = document.createElement("div");
-        newDiv.setAttribute("class", "card");
+
+        const newDiv = createCardElement();
+        
         container.appendChild(newDiv)
         
         const img = addImgToanElement(imgSrc);
         newDiv.appendChild(img);
     }
+}
+
+function createCardElement() {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "card");
+    return newDiv;
 }
 
 function addImgToanElement(src){
